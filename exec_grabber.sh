21890b00f03cebeaf4997bbdb867c0bbe96c6d0c
Lucsexe <lucas12exequiel@gmail.com>
2025-05-28 12:15:00 -0300

    Update exec_grabber.sh

--- exec_grabber.sh
+++ exec_grabber.sh
@@ -3,6 +3,7 @@ python3 -m pip install requests
 python3 -m pip install lxml
 python3 -m pip install pytz
 python3 -m pip install beautifulsoup4
+python3 -m pip install yt-dlp
 
 python3 grabber.py > ./streams.m3u8
 
